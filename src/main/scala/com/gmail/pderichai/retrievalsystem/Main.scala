630adece198892f4817d50ad9f9350f37713f5c9
isajay <jimenezi@seas.upenn.edu>
2016-11-23 17:33:58 +0100

    setup IntelliJ project

---
+++ src/main/scala/com/gmail/pderichai/retrievalsystem/Main.scala
@@ -0,0 +1,12 @@
+package com.gmail.pderichai.retrievalsystem
+
+/**
+  * Created by Isa on 11/23/2016.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("This is our retrieval system")
+  }
+
+}
