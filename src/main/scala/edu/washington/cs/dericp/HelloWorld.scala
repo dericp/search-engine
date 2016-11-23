4bb2fbc1b61139172defdf083b2c2f0a2da4c32b
Deric Pang <dericp@cs.washington.edu>
2016-11-23 22:42:15 +0100

    changing scala version

---
+++ src/main/scala/edu/washington/cs/dericp/HelloWorld.scala
@@ -0,0 +1,11 @@
+package edu.washington.cs.dericp
+
+/**
+  * Created by dericp on 11/23/16.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
