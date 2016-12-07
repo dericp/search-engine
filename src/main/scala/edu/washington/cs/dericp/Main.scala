e75ef0b9130904c65ff7d12b7c4802497fb76b04
isajay <jimenezi@seas.upenn.edu>
2016-12-07 16:24:30 +0100

    fixed language model for string ids

--- src/main/scala/edu/washington/cs/dericp/Main.scala
+++ src/main/scala/edu/washington/cs/dericp/Main.scala
@@ -30,6 +30,7 @@ object Main {
 //    val invInd = new InvertedIndex(docs).invertedIndex
 //    val docLengths = docs.map(doc => (doc.name -> doc.tokens.length)).toMap
 //    val termModel = new TermModel(invInd, docLengths)
+//    val langModel = new LanguageModel(invInd, docLengths)
 //
 //    // common words: society, cages, 000
 //    val doc1 = docs(0)
@@ -37,13 +38,15 @@ object Main {
 //    val doc2 = docs(1)
 //    println("doc1 name: " + doc1.name)
 //
-//    // val query = List("his", "hampshire", "dole", "buckley")
-//    val query = List("society", "cages", "000")
+//    val query = List("his", "hampshire", "dole", "buckley")
+//    //val query = List("society", "cages", "000")
 //
 //    // println("score 1 (should be low): " + termModel.tfIdfScore(query, doc1.name))
 //    // println("score 2 (should be higher): " + termModel.tfIdfScore(query, doc2.name))
 //
-//    println(termModel.topNDocs(query, 15))
+//    //println(termModel.topNDocs(query, 15))
+//    //println(langModel.findLogPQDSmooth(query, doc1.name, .01).toString)
+//    //println(langModel.findLogPQDSmooth(query, doc2.name, .01).toString)
   }
 }
 
