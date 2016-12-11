9f919966c91057a81834f92570c164fb00de5939
isajay <jimenezi@seas.upenn.edu>
2016-12-11 16:41:06 +0100

    use docdata for term model adn termmodeltest

---
+++ src/main/scala/edu/washington/cs/dericp/RetrievalModel.scala
@@ -0,0 +1,8 @@
+package edu.washington.cs.dericp
+
+/**
+  * Created by Isa on 12/11/2016.
+  */
+class RetrievalModel {
+
+}
--- src/main/scala/edu/washington/cs/dericp/TermModel.scala
+++ src/main/scala/edu/washington/cs/dericp/TermModel.scala
@@ -9,7 +9,7 @@ import breeze.util.TopK
   * UPDATE TERM MODEL TEST FILE
   *
   */
-class TermModel(val index: Map[String,List[(String,Int)]], val docLength: Map[String,Int]) {
+class TermModel(val index: Map[String,List[DocData]], val docLength: Map[String,Int]) extends RetrievalModel {
   // TODO: Normalize term frequency by max tf in document
 
   // Document frequencies = index.mapValues(_ => _.length)
@@ -21,12 +21,19 @@ class TermModel(val index: Map[String,List[(String,Int)]], val docLength: Map[St
   // Using simple scoring - sum of tf-idf scores of each query word, log of tf
   // TODO: Convert to use vector-based model and more complex scoring?
   def tfIdfScore(query: List[String], docID: String): Double = {
-    val ltf = query.map( term => term -> Math.log(1 + index(term).find(_._1 == docID).getOrElse((0, 0))._2)).toMap
+    val ltf = query.map( term => term -> Math.log(1 + index(term).find(_.id() == docID).getOrElse(new DocData("0", 0)).freq)).toMap
     ltf.map{ case(term, ltf) => ltf * idf.getOrElse(term, 0.0) }.sum
   }
 
   // TODO: Returns the top
-  def topNDocs(query: List[String], n: Int, shortenedDocList: Seq[String]): List[String] = {
+  // TODO: get list intersection
+  def topNDocs(query: List[String], n: Int/*, shortenedDocList: Seq[String]*/): List[String] = {
+    val containAllQueryWords = InvertedIndex.listIntersection(query, index)
+    var shortenedDocList = docLength.keys
+    println(containAllQueryWords.size)
+    if (containAllQueryWords.size >= n) {
+      shortenedDocList = shortenedDocList.filter(d => containAllQueryWords.contains(d))
+    }
     val tfIdfScores = shortenedDocList.map(docID => (docID, tfIdfScore(query, docID))).toList
     tfIdfScores.sortBy(-_._2).take(n).map(_._1)
   }
