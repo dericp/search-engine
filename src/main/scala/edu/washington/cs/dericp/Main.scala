72444c1f359b01b0246ebb6f59f521e5e4b836f4
isajay <jimenezi@seas.upenn.edu>
2016-12-11 16:51:16 +0100

    edit Main to take multiple queries

--- src/main/scala/edu/washington/cs/dericp/Main.scala
+++ src/main/scala/edu/washington/cs/dericp/Main.scala
@@ -8,7 +8,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println("Which relevance model would you like to use? LANGUAGE or TERM:")
-    val model = StdIn.readLine()
+    val model = StdIn.readLine().toLowerCase
     println("Using " + model + " model.")
     println()
 
@@ -30,7 +30,35 @@ object Main {
       }
     }
 
-    // TODO: score documents and return top n
+    // TODO: figure out the other case?
+    val retrievalModel = model match {
+      case "language" => new LanguageModel(invIdx, Map.empty, .01)
+      case "term" => new TermModel(invIdx, Map.empty)
+      case _ => new LanguageModel(invIdx, Map.empty, .01)
+    }
+
+    var keepQuerying = true
+
+    while (keepQuerying) {
+      // TODO: might want to delete query option from above
+      println("Please enter your query:")
+      val query = StdIn.readLine().split("\\s+").map(term => PorterStemmer.stem(term)).toList
+      println()
+
+      // COMPUTE WITH MODEL AND PRINT RESULTS
+      val results = retrievalModel.topNDocs(query, 100)
+      println(results)
+      println()
+
+      println("Would you like to enter another query? y/n:")
+      val response = StdIn.readLine()//.startsWith("y")
+      if (response.startsWith("n")) {
+        keepQuerying = false
+      }
+    }
+
+    // TODO: score documents
+
   }
 }
 
