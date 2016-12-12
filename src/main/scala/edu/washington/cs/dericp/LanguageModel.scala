d5b7c380cc6ba23722d8d27f9d95c6773bd55c52
isajay <jimenezi@seas.upenn.edu>
2016-12-12 22:17:00 +0100

    commenting in LanguageModel

--- src/main/scala/edu/washington/cs/dericp/LanguageModel.scala
+++ src/main/scala/edu/washington/cs/dericp/LanguageModel.scala
@@ -9,22 +9,39 @@ import scala.math.log
   */
 class LanguageModel(val index: Map[String, Seq[DocData]], val docLengths: Map[String,Int], lambda: Double)
   extends RelevanceModel {
-  // currently a unigram model
 
+  /**
+    * Takes in a docID and a list of DocData objects and returns the DocData containing the matching the docID
+    *
+    * @param doc
+    * @param list
+    * @return matching DocData
+    */
   def getDocDataFromList(doc: String, list: Seq[DocData]) : DocData = {
     val filtered = list.filter(dd => dd.id() == doc)
     // technically should always work bc id should only exist once, but be careful
     filtered.head
   }
 
-  // find P(w)
+  /**
+    * Finds the probability of the word in the entire index
+    *
+    * @param word
+    * @return P(w)
+    */
   def findPW(word: String) : Double = {
     val wordFreq = index.getOrElse(word, List.empty).map(_.freq).sum
     val corpusSize = docLengths.values.sum
     wordFreq.toDouble / corpusSize
   }
 
-  // find log(P(w|d))
+  /**
+    * Finds the probability of a certain word given a specific document
+    *
+    * @param word
+    * @param doc
+    * @return  P(w|d)
+    */
   def findPWD(word: String, doc: String) : Double = {
     val docsWithFreqs = index.getOrElse(word, List.empty)
     // need to add smoothing to fix 0 freq, and fix doc length = 0 (applies for non smoothed below)
@@ -32,19 +49,27 @@ class LanguageModel(val index: Map[String, Seq[DocData]], val docLengths: Map[St
     tf.toDouble / docLengths.getOrElse(doc, 0)
   }
 
-  // TODO: probably delete this later!!!!!
-  // find P(q|d)
-  def findLogPQD(query: Seq[String], doc: String) : Double = {
-    query.map(w => log(findPWD(w, doc))).sum
-  }
-
-  // find P(q|d) with Jelinek-Mercer smoothing
+  /**
+    * Find the probability of a certain query given a specific document using Jelinek-Mercer smoothing
+    *
+    * @param query
+    * @param doc
+    * @param lambda
+    * @return P(q|d)
+    */
   def findLogPQDSmooth(query: Seq[String], doc: String, lambda: Double) : Double = {
     val dqIntersection = query.filter(w => !index.getOrElse(w, List.empty).filter(a => a.id() == doc).isEmpty)
     val termProbs = dqIntersection.map(w => log(1 + ((1 - lambda) / lambda) * (findPWD(w, doc) / findPW(w)))).sum
     termProbs + log(lambda)
   }
 
+  /**
+    * Returns an ordered list of the top n relevant docs for a given query
+    *
+    * @param query
+    * @param n
+    * @return search results
+    */
   def topNDocs(query: Seq[String], n: Int): Seq[String] = {
     println("begin trimming index")
     val trimmedIndex = InvertedIndex.listIntersection(query, n, index)
